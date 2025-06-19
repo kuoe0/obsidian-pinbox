c95103f0532bd777b328b587001c40621c75052b
kuoe0 <kuoe0.tw@gmail.com>
2025-06-19 11:53:34 +0800

    Implement showing bookmarked notes in the share menu

--- src/modals.ts
+++ src/modals.ts
@@ -1,14 +1,11 @@
 import { App, FuzzySuggestModal, TFile, Notice } from "obsidian";
-import PinboxPlugin from "./main";
 
 export class NoteSuggesterModal extends FuzzySuggestModal<TFile> {
-	plugin: PinboxPlugin;
-	onSelectCallback: () => void;
+	onChooseFileCallback: (file: TFile) => Promise<void>;
 
-	constructor(app: App, plugin: PinboxPlugin, onSelect: () => void) {
+	constructor(app: App, onChooseFile: (file: TFile) => Promise<void>) {
 		super(app);
-		this.plugin = plugin;
-		this.onSelectCallback = onSelect;
+		this.onChooseFileCallback = onChooseFile;
 		this.setPlaceholder("Search for a note to pin...");
 	}
 
@@ -21,13 +18,6 @@ export class NoteSuggesterModal extends FuzzySuggestModal<TFile> {
 	}
 
 	async onChooseItem(item: TFile): Promise<void> {
-		if (this.plugin.settings.pinnedNotePaths.includes(item.path)) {
-			new Notice(`${item.basename} is already pinned.`);
-			return;
-		}
-		this.plugin.settings.pinnedNotePaths.push(item.path);
-		await this.plugin.saveSettings();
-		new Notice(`Pinned "${item.basename}"`);
-		this.onSelectCallback();
+		await this.onChooseFileCallback(item);
 	}
 }
