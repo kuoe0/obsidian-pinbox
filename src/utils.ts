f9fac5fc5c1e392e06908d4ed7e4394ecf23c9cf
Chih-Hsuan Kuo <kuoe0.tw@gmail.com>
2025-06-21 03:45:57 +0800

    [Refactor] Make FormatEditor use options object for constructor parameters

--- src/utils.ts
+++ src/utils.ts
@@ -5,19 +5,19 @@
  * @returns The processed string with placeholders replaced.
  */
 export function processPlaceholders(format: string, content: string): string {
-	const now = new Date();
-	const date = `${now.getFullYear()}-${(now.getMonth() + 1)
-		.toString()
-		.padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}`;
-	const time = `${now.getHours().toString().padStart(2, "0")}:${now
-		.getMinutes()
-		.toString()
-		.padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}`;
-	const timestamp = `${date} ${time}`;
+  const now = new Date();
+  const date = `${now.getFullYear()}-${(now.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}`;
+  const time = `${now.getHours().toString().padStart(2, "0")}:${now
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}`;
+  const timestamp = `${date} ${time}`;
 
-	return format
-		.replace(/{{content}}/g, content)
-		.replace(/{{timestamp}}/g, timestamp)
-		.replace(/{{date}}/g, date)
-		.replace(/{{time}}/g, time);
+  return format
+    .replace(/{{content}}/g, content)
+    .replace(/{{timestamp}}/g, timestamp)
+    .replace(/{{date}}/g, date)
+    .replace(/{{time}}/g, time);
 }
